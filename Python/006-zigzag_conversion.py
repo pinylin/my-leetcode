981a6bfc90cb59dd518a35a9d2a4bfc334a1f6d0
Ping-linlin <780319308@qq.com>
2018-01-18 13:39:55 +0800

    006

---
+++ Python/006-zigzag_conversion.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     006-zigzag_conversion
+   Description :
+   Author :       pinglin
+   date：          18-1-18
+-------------------------------------------------
+   Change Activity:
+                   18-1-18:
+-------------------------------------------------
+"""
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
