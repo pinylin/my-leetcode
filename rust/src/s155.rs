39e42a586be4ac700282238f2ee1d69c9918a174
pinylin <pinglinvip@gmail.com>
2019-04-13 11:55:43 +0800

    min stack

---
+++ rust/src/s155.rs
@@ -0,0 +1,121 @@
+/*
+ * @lc app=leetcode id=155 lang=rust
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (36.19%)
+ * Total Accepted:    284.9K
+ * Total Submissions: 783.8K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+struct MinStack {
+    stack: Vec<i32>,
+    min: i32,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+#[allow(dead_code)]
+impl MinStack {
+
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        MinStack {
+            stack: Vec::new(),
+            min: std::i32::MAX,
+        }
+    }
+    // 关键在于push和pop 当x<= min时，push min,x 两个数
+    fn push(&mut self, x: i32) {
+        if x <= self.min {
+            self.stack.push(self.min);
+            self.min = x;
+        }
+        self.stack.push(x);
+    }
+    // 若 pop出的数= min,则self.min = 下一次pop出的数
+    fn pop(&mut self) {
+        match self.stack.pop() {
+            Some(i) => {
+                if i == self.min {
+                    self.min = self.stack.pop().unwrap()
+                }
+            },
+            None => { },
+        };
+    }
+    
+    fn top(&self) -> i32 {
+        *self.stack.last().unwrap()
+    }
+    
+    fn get_min(&self) -> i32 {
+        self.min
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack::new();
+ * obj.push(x);
+ * obj.pop();
+ * let ret_3: i32 = obj.top();
+ * let ret_4: i32 = obj.get_min();
+ */
+
+
+
+#[cfg(test)]
+mod test {
+    use super::MinStack;
+
+    #[test]
+    fn it_works() {
+        let mut min_stack = MinStack::new();
+        min_stack.push(-2);
+        min_stack.push(0);
+        min_stack.push(-3);
+        assert_eq!(min_stack.get_min(), -3);
+        min_stack.pop();
+        assert_eq!(min_stack.top(), 0);
+        assert_eq!(min_stack.get_min(), -2);
+    }
+}
