5b11141307fc81facde1c8a1cb33219850905966
pinylin <wangjp@1391.com>
2019-04-16 15:48:47 +0800

    factorial trailing zeroes

---
+++ rust/src/s172.rs
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=172 lang=rust
+ *
+ * [172] Factorial Trailing Zeroes
+ *
+ * https://leetcode.com/problems/factorial-trailing-zeroes/description/
+ *
+ * algorithms
+ * Easy (37.27%)
+ * Total Accepted:    152.2K
+ * Total Submissions: 407.8K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 3
+ * Output: 0
+ * Explanation: 3! = 6, no trailing zero.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 5
+ * Output: 1
+ * Explanation: 5! = 120, one trailing zero.
+ * 
+ * Note: Your solution should be in logarithmic time complexity.
+ * 
+ */
+impl Solution {
+    pub fn trailing_zeroes(mut n: i32) -> i32 {
+        let mut count = 0;
+        while n > 0 {
+            n = n / 5;
+            count += n;
+        }
+        count
+    }
+}
+
+pub struct Solution;
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::trailing_zeroes(5), 1);
+        assert_eq!(Solution::trailing_zeroes(1000), 249);
+    }
+}
