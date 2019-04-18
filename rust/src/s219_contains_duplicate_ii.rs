cf8dd41e61c90800b90d710840ba904c4fb9faec
pinylin <wangjp@1391.com>
2019-04-18 17:16:35 +0800

    s219

--- rust/src/s219_contains_duplicate_ii.rs
+++ rust/src/s219_contains_duplicate_ii.rs
@@ -47,7 +47,7 @@ impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
         use std::collections::HashMap;
         let mut map = HashMap::new();
-        
+
         for (i, num) in nums.iter().enumerate() {
             if let Some(j) = map.insert(num, i) {
                 if (i as i32) - (j as i32) <= k {
@@ -71,13 +71,7 @@ mod tests {
             Solution::contains_nearby_duplicate(vec![1, 2, 3, 1], 3),
             true
         );
-        assert_eq!(
-            Solution::contains_nearby_duplicate(vec![2, 2], 3),
-            true
-        );
-        assert_eq!(
-            Solution::contains_nearby_duplicate(vec![99, 99], 2),
-            true
-        );
+        assert_eq!(Solution::contains_nearby_duplicate(vec![2, 2], 3), true);
+        assert_eq!(Solution::contains_nearby_duplicate(vec![99, 99], 2), true);
     }
 }
