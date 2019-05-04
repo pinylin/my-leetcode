da55b1c4d1214990ab015381a99dfeef690bdf19
pinylin <pinglinvip@gmail.com>
2019-05-04 14:54:27 +0800

    modify

--- rust/src/s448_find_all_numbers_disappeared_in_array.rs
+++ rust/src/s448_find_all_numbers_disappeared_in_array.rs
@@ -49,7 +49,7 @@ mod test {
     #[test]
     fn it_works() {
         assert_eq!(
-            Solution::find_disappeared_numbers_best(vec![4, 3, 2, 7, 8, 2, 3, 1]),
+            Solution::find_disappeared_numbers(vec![4, 3, 2, 7, 8, 2, 3, 1]),
             vec![5, 6]
         );
     }
