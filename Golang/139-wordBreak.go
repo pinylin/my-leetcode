d736307e705c7a8477308c27040d256cc2f9bc3a
pinylin <pinglinvip@gmail.com>
2019-09-13 11:46:51 +0800

    139

--- Golang/139-wordBreak.go
+++ Golang/139-wordBreak.go
@@ -10,3 +10,38 @@
                 
 */
 package main
+
+import "fmt"
+
+func wordBreak(s string, wordDict []string) bool {
+	result := make(map[string]bool)
+	return helper(s, wordDict, result)
+}
+func helper(s string, wordDict []string, result map[string]bool) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	if ret, ok := result[s]; ok {
+		return ret
+	}
+
+	for _, word := range wordDict {
+		if len(word) > len(s) || word != s[:len(word)] {
+			continue
+		}
+
+		if helper(s[len(word):], wordDict, result) {
+			result[s] = true
+			return true
+		}
+	}
+
+	result[s] = false
+	return false
+}
+
+func main() {
+	// s = "applepenapple", wordDict = ["apple", "pen"]
+	fmt.Println(wordBreak("applepenapple", []string{"apple", "pen"}))
+}
