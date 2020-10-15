c8bd43342948fc55ad9eae10e46b2a54514ac503
pinylin <pinglinvip@gmail.com>
2020-10-15 19:16:34 +0800

    s349

--- rust/src/s349_intersection_of_two_arrays.rs
+++ rust/src/s349_intersection_of_two_arrays.rs
@@ -1,35 +1,45 @@
 /*
- * @lc app=leetcode id=349 lang=rust
+ * @lc app=leetcode.cn id=349 lang=rust
  *
- * [349] Intersection of Two Arrays
+ * [349] 两个数组的交集
  */
+
+// @lc code=start
+use std::collections::HashSet;
 impl Solution {
-    pub fn intersection(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> Vec<i32> {
+    
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut iset = HashSet::new();
+        let mut uset = HashSet::new();
+
+        for i in nums1 {
+            uset.insert(i);
+        }
+        for i in nums2 {
+            if uset.contains(&i) {
+                iset.insert(i);
+            }
+        }
+        iset.into_iter().collect()
+    }
+
+    pub fn intersection_sort(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> Vec<i32> {
         let mut nums = Vec::new();
         nums1.sort();
         nums2.sort();
         let mut i = 0;
         let mut j = 0;
         while i < nums1.len() && j < nums2.len() {
-            if nums1[i] > nums2[j] {
-                j += 1;
-                continue;
-            }
-            if nums1[i] == nums2[j] {
-                nums.push(nums1[i]);
-                i += 1;
-                j += 1;
-                continue;
-            }
-            if nums1[i] < nums2[j] {
-                i += 1;
-                continue;
-            }
+            if nums1[i] > nums2[j] { j += 1; continue; }
+            if nums1[i] == nums2[j] { nums.push(nums1[i]); i += 1; j += 1; continue; }
+            if nums1[i] < nums2[j] { i += 1; continue; }
         }
         nums.dedup();
         nums
     }
 }
+// @lc code=end
+
 
 pub struct Solution;
 
@@ -39,9 +49,7 @@ mod test {
     use super::Solution;
     #[test]
     fn it_works() {
-        assert_eq!(
-            Solution::intersection(vec![1, 2, 4, 9], vec![3, 5, 9]),
-            vec![9]
-        );
+        assert_eq!(Solution::intersection(vec![1,2,2,1], vec![2,2]), vec![2]);
+        assert_eq!(Solution::intersection_sort(vec![1,2,2,1], vec![2,2]), vec![2]);
     }
 }
