67204b6d9d7664154f2c77e3fc83447f1cd74e66
pinylin <pinglinvip@gmail.com>
2020-11-16 11:13:04 +0800

    s1475

---
+++ rust/src/s1475_final_prices.rs
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1475 lang=rust
+ *
+ * [1475] 商品折扣后的最终价格
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn final_prices(prices: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![0; prices.len()];
+        (0..prices.len()).for_each(|i| {
+            res[i] = prices[i]
+                - ((i + 1)..prices.len())
+                    .find(|&j| prices[j] <= prices[i])
+                    .map_or(0, |j| prices[j]);
+        });
+        res
+    }
+}
+// @lc code=end
+pub struct Solution;
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    // use crate::utils::vec2d;
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::final_prices(vec![10, 1, 1, 6]), vec![9, 0, 1, 6])
+    }
+}
