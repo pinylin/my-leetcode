84d534c1408b91c82f038e314a28f5614e91f908
pinylin <pinglinvip@gmail.com>
2020-11-16 11:25:11 +0800

    s1470

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -194,6 +194,12 @@ pub mod s541_reverse_string_ii;
 
 pub mod s1463_cherry_pick_ii;
 
+pub mod s1470_shuffle_arr;
+
+pub mod s1475_final_prices;
+
+pub mod s1480_running_sum;
+
 pub mod s1486_xor_operation;
 
 pub mod s1491_average_salary;
---
+++ rust/src/s1470_shuffle_arr.rs
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1470 lang=rust
+ *
+ * [1470] 重新排列数组
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+        let mut res = Vec::with_capacity(2 * n);
+        for i in 0..2 * n {
+            if i % 2 == 0 {
+                res.push(nums[i / 2]);
+            } else {
+                res.push(nums[n + i / 2]);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
+pub struct Solution;
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    // use crate::utils::vec2d;
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Solution::shuffle(vec![1, 2, 3, 4, 4, 3, 2, 1], 4),
+            vec![1, 4, 2, 3, 3, 2, 4, 1]
+        )
+    }
+}
