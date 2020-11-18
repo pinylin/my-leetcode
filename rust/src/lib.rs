6f514ddbe4ee53e522667f50f873f11d15158e29
pinylin <pinglinvip@gmail.com>
2020-11-18 20:26:04 +0800

    s1408

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -192,6 +192,8 @@ pub mod s538_convert_bst_greater_tree;
 
 pub mod s541_reverse_string_ii;
 
+pub mod s1408_string_matching_in_an_array;
+
 pub mod s1413_min_start_value;
 
 pub mod s1417_reformat_string;
---
+++ rust/src/s1408_string_matching_in_an_array.rs
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=1408 lang=rust
+ *
+ * [1408] 数组中的字符串匹配
+ */
+
+// @lc code=start
+use std::collections::HashSet;
+impl Solution {
+    pub fn string_matching(words: Vec<String>) -> Vec<String> {
+        let mut set: HashSet<String> = HashSet::new();
+        let mut words = words.to_vec();
+        words.sort_unstable_by(|a, b| a.len().cmp(&b.len()));
+        for (i, w) in words.iter().enumerate() {
+            for j in i + 1..words.len() {
+                if words[j].contains(w) {
+                    set.insert(w.to_string());
+                }
+            }
+        }
+        set.into_iter().collect::<Vec<String>>()
+    }
+}
+// @lc code=end
+
+pub struct Solution;
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    use crate::vec_string;
+    // use crate::utils::vec2d;
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Solution::string_matching(vec_string!["blue", "green", "bu"]),
+            vec![""; 0]
+        );
+        assert_eq!(
+            Solution::string_matching(vec_string!["mass", "as", "hero", "superhero"]),
+            vec_string!["as", "hero"]
+        );
+    }
+}
