33e124acf0ab94b6749ae6bdbde9dd2bf9a32a0c
pinylin <pinglinvip@gmail.com>
2020-12-11 16:14:11 +0800

    s1304

--- rust/src/s1304_sum_of_n_nums_is_zero.rs
+++ rust/src/s1304_sum_of_n_nums_is_zero.rs
@@ -7,8 +7,27 @@
 // @lc code=start
 impl Solution {
     pub fn sum_zero(n: i32) -> Vec<i32> {
-
+        let mut res: Vec<i32> = Vec::new();
+        if n % 2 == 1 {
+            res.push(0);
+        }
+        for i in 1..n / 2 + 1 {
+            res.push(i);
+            res.push(i * -1);
+        }
+        res
     }
 }
 // @lc code=end
 
+pub struct Solution;
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::sum_zero(1).iter().sum::<i32>(), 0);
+        assert_eq!(Solution::sum_zero(5).iter().sum::<i32>(), 0);
+    }
+}
