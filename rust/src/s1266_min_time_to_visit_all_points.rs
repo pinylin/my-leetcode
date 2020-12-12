fa8418bd668839736b15f6b14b635c1a7a868f27
pinylin <pinglinvip@gmail.com>
2020-12-12 11:09:44 +0800

    s1266

--- rust/src/s1266_min_time_to_visit_all_points.rs
+++ rust/src/s1266_min_time_to_visit_all_points.rs
@@ -7,20 +7,20 @@
 // @lc code=start
 impl Solution {
     pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
-        // let mut steps = 0;
-        // for i in 1..points.len() {
-        //     steps += std::cmp::max(
-        //         (points[i][0] - points[i - 1][0]).abs(),
-        //         (points[i][1] - points[i - 1][1]).abs(),
-        //     );
-        // }
-        // steps
+        let mut steps = 0;
+        for i in 1..points.len() {
+            steps += std::cmp::max(
+                (points[i][0] - points[i - 1][0]).abs(),
+                (points[i][1] - points[i - 1][1]).abs(),
+            );
+        }
+        steps
 
         // 方法2  参考 1566 注意windows和chunks不同
-        points
-            .windows(2)
-            .map(|x| std::cmp::max((x[1][0] - x[0][0]).abs(), (x[1][1] - x[0][1]).abs()))
-            .sum()
+        // points
+        //     .windows(2)
+        //     .map(|x| std::cmp::max((x[1][0] - x[0][0]).abs(), (x[1][1] - x[0][1]).abs()))
+        //     .sum()
     }
 }
 // @lc code=end
