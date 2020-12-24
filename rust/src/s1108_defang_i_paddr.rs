d2e82f7c8d1532b63ef9e901482dde799f4322c6
pinylin <pinglinvip@gmail.com>
2020-12-24 15:44:52 +0800

    s1108

---
+++ rust/src/s1108_defang_i_paddr.rs
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1108 lang=rust
+ *
+ * [1108] IP 地址无效化
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn defang_i_paddr(address: String) -> String {
+        address.replace('.', "[.]")
+    }
+}
+// @lc code=end
+
+pub struct Solution;
